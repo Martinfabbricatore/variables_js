f65e04045d76a3aa33f5aa9fe59faffa35123ca6
Martinfabbricatore <102688066+Martinfabbricatore@users.noreply.github.com>
2022-06-18 08:30:35 -0300

    Add files via upload

---
+++ ejercicios_practica/js/ejercicios_practica/js/ejercicio_1.js
@@ -0,0 +1,27 @@
+"use strict";
+
+/* Tarea
+ * Objetivos: adquirir herramientas para identificar 
+ * problemas en el código, identificando si se producen
+ * errores de sintaxis o de lógica.
+ * debe descomentar cada ejercicio!
+ */
+
+// 1. Corrija los errores presentes en el código:
+
+/* ELIMINE ESTE COMENTARIO
+const nom bre = " otros";
+const accion "Programamos";
+const negacion = ", no ";
+para " para "
+quien = "nosotros mismos"
+
+console . log(accion + para + nombre + negacion + para + quien);
+*/
+const NOMBRE = " otros "; 
+const ACCION = " Programamos ";
+const NEGACION = " no ";
+const PARA = " para ";
+const QUIEN = " nosotros mismos ";
+
+console . log(ACCION + PARA + NOMBRE + NEGACION + PARA + QUIEN);
---
+++ ejercicios_practica/js/ejercicios_practica/js/ejercicio_4.js
@@ -0,0 +1,37 @@
+"use strict";
+
+/* Tarea
+ * Objetivos: adquirir herramientas para identificar 
+ * problemas en el código, identificando si se producen
+ * errores de sintaxis o de lógica.
+ * debe descomentar cada ejercicio!
+ */
+ 
+ //-------------------------------------------------------------------
+// 4. Realice un acumulador utilizando una variable, debe acumular en 5 lineas
+//    de código el numero 100, comenzando desde el 1.
+
+/* ELIMINE ESTE COMENTARIO
+let acumulador = 1; //No utilizar mas variables ;D
+//primer paso:
+
+//segundo paso:
+
+//tercer paso:
+
+//cuarto paso:
+
+//quinto paso:
+
+//Imprimimos en consola:
+console.log("El resultado del programa es: " + acumulador); 
+// El resultado debe ser 100
+*/
+let acumulador = 1;
+acumulador = acumulador + 4;
+acumulador = acumulador + 5;
+acumulador = acumulador + 20;
+acumulador = acumulador + 30;
+acumulador = acumulador + 40;
+
+console.log("El resultado de programa es:"+ acumulador)
